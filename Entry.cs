54ecd3391267c6a4b4883a638d9b39bfc7a3d021
Ronny Chan <ronny6993@gmail.com>
2015-07-08 23:20:53 -0400

    Remove protobuf

--- Entry.cs
+++ Entry.cs
@@ -3,27 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ProtoBuf;
 
 namespace awagame
 {
     [Serializable]
-    [ProtoContract]
     internal class Entry
     {
-        [ProtoMember(1)]
         public string GameName { get; set; }
-        [ProtoMember(2)]
         public string RomFileName { get; set; }
-        [ProtoMember(3)]
         public string RomSize { get; set; }
-        [ProtoMember(4)]
         public string HashCRC32 { get; set; }
-        [ProtoMember(5)]
         public string HashMD5 { get; set; }
-        [ProtoMember(6)]
         public string HashSHA1 { get; set; }
-        [ProtoMember(7)]
         public string OpenVGDB_RomID { get; set; }
 
     }
