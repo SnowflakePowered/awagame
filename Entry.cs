47d05e863e8a93ad5cb8be68d947f782fab00a0b
Ronny Chan <ronny6993@gmail.com>
2015-07-08 20:11:03 -0400

    Initial Commit

---
+++ Entry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace awagame
+{
+    internal class Entry
+    {
+        public string GameName { get; set; }
+        public string RomName { get; set; }
+        public string RomSize { get; set; }
+        public string HashCRC32 { get; set; }
+        public string HashMD5 { get; set; }
+        public string HashSHA1 { get; set; }
+
+    }
+}
---
+++ awaseru.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Data.SQLite;
+using System.Net;
+namespace awagame
+{
+    public class awaseru
+    {
+        
+        public static void BuildCurrentDirectory()
+        {
+            string directory = Environment.CurrentDirectory;
+            var combine = new awaseru(directory);
+        }
+
+        string CurrentDirectory;
+        List<string> DatFilesPath;
+        List<XDocument> DatFiles;
+        SQLiteConnection Database;
+
+        private awaseru(string directory)
+        {
+            this.CurrentDirectory = directory;
+            this.DatFilesPath = Directory.EnumerateFiles(this.CurrentDirectory).Where(filename => Path.GetExtension(filename) == ".dat").ToList();
+            this.DatFiles = this.DatFilesPath.Select(filePath => XDocument.Load(File.OpenRead(filePath))).ToList();
+        }
+    }
+}
